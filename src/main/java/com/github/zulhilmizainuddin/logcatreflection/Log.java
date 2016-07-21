42e8727bd77910816a5fc025393677fbd317885e
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-21 19:02:57 +0800

    Initial commit

---
+++ src/main/java/com/github/zulhilmizainuddin/logcatreflection/Log.java
@@ -0,0 +1,42 @@
+package com.github.zulhilmizainuddin.logcatreflection;
+
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Log {
+    public static void v(String tag, String log) {
+        log("v", tag, log);
+    }
+
+    public static void d(String tag, String log) {
+        log("d", tag, log);
+    }
+
+    public static void i(String tag, String log) {
+        log("i", tag, log);
+    }
+
+    public static void w(String tag, String log) {
+        log("w", tag, log);
+    }
+
+    public static void e(String tag, String log) {
+        log("e", tag, log);
+    }
+
+    private static void log(String method, String tag, String log) {
+        try {
+            Class<?> logClass = Class.forName("android.util.Log");
+            Method infoMethod = logClass.getDeclaredMethod(method, String.class, String.class);
+
+            infoMethod.invoke(null, tag, log);
+        }
+        catch (ClassNotFoundException |
+                NoSuchMethodException |
+                IllegalAccessException |
+                InvocationTargetException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
